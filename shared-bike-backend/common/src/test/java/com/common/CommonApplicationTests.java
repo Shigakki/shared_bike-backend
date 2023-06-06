f0ef0a09634b8bcf267ba2758187c8b376714a5f
DruncBread <2146015487@qq.com>
2023-06-06 11:21:17 +0800

    socket_test_ok

--- shared-bike-backend/common/src/test/java/com/common/CommonApplicationTests.java
+++ shared-bike-backend/common/src/test/java/com/common/CommonApplicationTests.java
@@ -1,13 +1,2 @@
 package com.common;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CommonApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
